510f4a4b7959d5f3ddbc846131a7be8bfa2cdcc3
borisenko_nick <016@rambler.ru>
2021-06-23 13:52:15 +0300

    change the cascade_of_components_appearance.vue

--- cascade_of_components_appearance/cascade_of_components_appearance.vue
+++ cascade_of_components_appearance/cascade_of_components_appearance.vue
@@ -1,39 +1,84 @@
 <template>
+<!-- должен быть ТРИГГЕР(!), который присуждает ИНОЕ ЗНАЧЕНИЕ для CSS-свойства. Или :hover, или from JS... -->
+<!--  v-1-->
+<!-- ТРИГГЕР - изменение степени скрулла страницы отлавливается by npm-пакетом "vue-check-view", и далее дочкам добавляется новый CSS-класс. -->
   <div v-view="viewHandler" class="container">
     <div class="cleaners">
       <div v-for="(cleaner, index) in 4" :key="index"
-           class="cleaner">
+           class="cleaner"
+      >
         go! - {{index}}
       </div>
     </div>
   </div>
 </template>
+
+<!--v-2-->
+<!-- ТРИГГЕР - команда via JS by @click, которая присуждает runCascadeAppearance=true. -->
+<template>
+  <div class="container">
+    <div class="cleaners" :class="{'show': runCascadeAppearance}">
+      <div v-for="(cleaner, index) in 4" :key="index"
+           class="cleaner"
+      >
+        go! - {{index}}
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<!--v-3-->
+<!-- ТРИГГЕР - в mounted() -->
+<div class="wrapper" :class="{'show': runCascadeAppearance}" :key="`${$route.params.shelf}`">
+  <div v-for="(product, ind) in GET_PRODUCTS($route.params.shelf)"
+     :key="ind"
+     @click.exact="onGoToProductDescription(product._id)"
+     class="cart"
+  >
+    <product-cart :product="product"/>
+  </div>
+</div>
+
+
+
+
 <script>
   export default {
+    data: () => ({
+      runCascadeAppearance: false,  //for v-2   //for v-3
+    }),
     methods: {
-      viewHandler(e) {
-        const coeff = window.innerWidth < 1024 ? 0.1 : 0.3
-        if (e.percentInView > coeff) e.target.element.classList.add('show')
+      viewHandler(e) {    //for v-1
+        const coefficient = window.innerWidth < 1024 ? 0.1 : 0.3  //можно не изголяться и стандартно выбрать 0.2
+        
+        if (e.percentInView > coefficient)     // e.percentInView = 0..1, how much element overlap/перекрывает/ the viewport.
+          e.target.element.classList.add('show')
       }
+    },
+    mounted() {      //for v-3
+      setTimeout(() => this.runCascadeAppearance = true, 70)
     }
   }
 </script>
+
+
+
 <style lang="scss" scoped>
   @mixin tr-reset() {
     opacity: 1;
     transform: translate(0, 0);
   }
 
-  @function trns($h, $s...) {
-    $f: '';
-    @each $t in $s {
-      $f: $f + $t + ' ' + $h + ', ';
+  @function trns($how, $properties...) {
+    $result: '';
+    @each $property in $properties {                      //$properties - это ['opacity', 'transform'].
+      $result: $result + $property + ' ' + $how + ', ';
     }
-    @return #{$f};
+    @return #{$result};
   }
-  /*эта функция выдаст:*/
-  /*opacity linear .5s*/
-  /*transform  linear .5s*/
+  //эта функция выдаст:
+  //"opacity linear .5s, transform  linear .5s, "
 
   $base-delay: 0.2s;
   $OutCubic: cubic-bezier(0.215, 0.61, 0.355, 1);
--- cascade_of_components_appearance/vue-check-view.js
+++ cascade_of_components_appearance/vue-check-view.js
@@ -1,3 +1,4 @@
 import Vue from 'vue'
-import checkView from 'vue-check-view'
+import checkView from 'vue-check-view'   //отслеживает присутствие в ВИДИМОЙ зоне броузера определенной части страницы.
+                                         //https://github.com/vtimofeev/vue-check-view
 Vue.use(checkView)
